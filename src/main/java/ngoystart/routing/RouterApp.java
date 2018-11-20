0f88e7ff2e2c2cd75efd107fb59c404e1e7814e2
christian.oetterli <christian.oetterli@bison-group.com>
2018-11-20 11:59:52 +0100

    extract pipes

--- src/main/java/ngoystart/routing/RouterApp.java
+++ src/main/java/ngoystart/routing/RouterApp.java
@@ -1,7 +1,5 @@
 package ngoystart.routing;
 
-import static java.lang.String.format;
-
 import java.util.List;
 
 import ngoy.core.Component;
@@ -9,18 +7,14 @@ import ngoy.core.Inject;
 import ngoy.core.NgModule;
 import ngoy.router.Route;
 import ngoy.router.Router;
-import ngoy.translate.TranslateService;
 
 @Component(selector = "", templateUrl = "app.component.html")
-@NgModule(declarations = { LocaleParamPipe.class })
+@NgModule(declarations = { LocaleParamPipe.class, RouteTitlePipe.class })
 public class RouterApp {
 	public final String appName = "Router";
 
 	@Inject
 	public Router router;
-
-	@Inject
-	public TranslateService translateService;
 
 	public List<Route> getRoutes() {
 		return router.getRoutes();
@@ -29,10 +23,4 @@ public class RouterApp {
 	public boolean isActiveRoute(Route route) {
 		return router.isActive(route);
 	}
-
-	public String routeTitle(Route route) {
-		return translateService.translate(format("MSG_%s", route.getPath()
-				.toUpperCase()));
-	}
-
 }
