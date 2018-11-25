4e596464154d600c1556e2a4bb02b67c8d6b9c7a
Christian Oetterli <christian.oetterli@bison-group.com>
2018-11-25 10:20:31 +0100

    extract ngoy into tmp folder

--- ngoy.cmd
+++ ngoy.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set ngoyVersion=beta0
-set ngoyPath=build/ngoy-%ngoyVersion%
+set ngoyPath=build\tmp\ngoy-%ngoyVersion%
 
 if not exist %ngoyPath% (
 	echo Extracting ngoy binaries...
