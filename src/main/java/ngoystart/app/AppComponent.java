d834f8cbd42c5e4d67b538c559d4049e1906a516
Christian Oetterli <christian@o3tt3rli.com>
2019-06-13 00:13:42 +0200

    format

--- src/main/java/ngoystart/app/AppComponent.java
+++ src/main/java/ngoystart/app/AppComponent.java
@@ -2,7 +2,7 @@ package ngoystart.app;
 
 import ngoy.core.Component;
 
-@Component(selector = "", templateUrl = "app.component.html", styleUrls = { "app.component.css" })
+@Component(selector = "", templateUrl = "app.component.html", styleUrls = {"app.component.css"})
 public class AppComponent {
-	public final String title = "my-app";
+    public final String title = "my-app";
 }
--- src/main/java/ngoystart/app/Main.java
+++ src/main/java/ngoystart/app/Main.java
@@ -1,35 +1,34 @@
 package ngoystart.app;
 
-import javax.servlet.http.HttpServletResponse;
-
+import ngoy.Ngoy;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ngoy.Ngoy;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/*")
 public class Main implements InitializingBean {
 
-	private Ngoy<AppComponent> ngoy;
+    private Ngoy<AppComponent> ngoy;
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		createApp();
-	}
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        createApp();
+    }
 
-	private void createApp() {
-		ngoy = Ngoy.app(AppComponent.class)
-				.translateBundle("messages")
-				.build();
-	}
+    private void createApp() {
+        ngoy = Ngoy.app(AppComponent.class)
+                .translateBundle("messages")
+                .build();
+    }
 
-	@GetMapping()
-	public void home(HttpServletResponse response) throws Exception {
-		// re-create while developing to have changes picked-up
+    @GetMapping()
+    public void home(HttpServletResponse response) throws Exception {
+        // re-create while developing to have changes picked-up
 //		createApp();
-		ngoy.render(response.getOutputStream());
-	}
+        ngoy.render(response.getOutputStream());
+    }
 }
